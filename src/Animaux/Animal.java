cc95628ab6b3006c65c2c45770fcc00442ebc440
Poukilou <hamid.boudjerda@gmail.com>
2018-07-30 15:13:51 +0200

    test

---
+++ src/Animaux/Animal.java
@@ -0,0 +1,109 @@
+package Animaux;
+
+public class Animal {
+	Membre m = new Membre();
+	Classification c = new Classification();
+	String environnement;
+	String nourriture;
+	double taille;
+	String couleur;
+	String peau;
+	double poids;
+	int esperanceVie;
+	String modeReproduction;
+	
+	void seNourrir (String aliment) {
+		
+	}
+	
+	void Dormir () {
+		
+	}
+	
+	void seReveiller () {
+		
+	}
+	
+	void seReproduire() {
+		
+	}
+	
+	void seDeplacer() {
+		
+	}
+
+	public String getEnvironnement() {
+		return environnement;
+	}
+
+	public void setEnvironnement(String environnement) {
+		this.environnement = environnement;
+	}
+
+	public String getNourriture() {
+		return nourriture;
+	}
+
+	public void setNourriture(String nourriture) {
+		this.nourriture = nourriture;
+	}
+
+	public double getTaille() {
+		return taille;
+	}
+
+	public void setTaille(double taille) {
+		this.taille = taille;
+	}
+
+	public String getCouleur() {
+		return couleur;
+	}
+
+	public void setCouleur(String couleur) {
+		this.couleur = couleur;
+	}
+
+	public String getPeau() {
+		return peau;
+	}
+
+	public void setPeau(String peau) {
+		this.peau = peau;
+	}
+
+	public double getPoids() {
+		return poids;
+	}
+
+	public void setPoids(double poids) {
+		this.poids = poids;
+	}
+
+	public int getEsperanceVie() {
+		return esperanceVie;
+	}
+
+	public void setEsperanceVie(int esperanceVie) {
+		this.esperanceVie = esperanceVie;
+	}
+
+	public String getModeReproduction() {
+		return modeReproduction;
+	}
+
+	public void setModeReproduction(String modeReproduction) {
+		this.modeReproduction = modeReproduction;
+	}
+	
+
+	
+	/*
+	public static void main(String[] args) {
+		
+		
+		
+
+	}
+*/
+}
